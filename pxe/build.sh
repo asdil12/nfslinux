79d50be5f61ceeb4accdf512e9df6deee5e3da72
Dominik Heidler <dheidler@suse.de>
2023-12-15 22:25:18 +0100

    UEFI: Use snponly image (similar to undionly image on BIOS)

--- pxe/build.sh
+++ pxe/build.sh
@@ -3,10 +3,16 @@
 mkdir -p /srv/tftpboot/
 test -d ipxe || git clone https://github.com/ipxe/ipxe.git
 pushd ipxe/src
-make -j20 EMBED=../../boot.ipxe bin/ipxe.pxe bin/undionly.kpxe bin-x86_64-efi/ipxe.efi
-sudo cp -v bin/ipxe.pxe bin/undionly.kpxe bin-x86_64-efi/ipxe.efi /srv/tftpboot/
+make -j20 EMBED=../../boot.ipxe bin/ipxe.pxe bin/undionly.kpxe bin-x86_64-efi/ipxe.efi bin-x86_64-efi/snponly.efi
+sudo cp -v bin/ipxe.pxe bin/undionly.kpxe bin-x86_64-efi/ipxe.efi bin-x86_64-efi/snponly.efi /srv/tftpboot/
 popd
 sudo cp -v menu.ipxe /srv/tftpboot/
+
+# bin/ipxe.pxe uses iPXE network drivers on BIOS systems
+# bin/undionly.kpxe uses generic PXE UNDI pxe firmware network driver on BIOS systems (this should always work)
+# bin-x86_64-efi/ipxe.efi uses iPXE network drivers on UEFI systems
+# bin-x86_64-efi/snponly.efi uses generic PXE SNP network driver un UEFI systems (this should always work)
+# bin-x86_64-efi/snp.efi is similar to snponly.efi but would try all network cards - not only the one it was chainloaded via
 
 # if you want to use PXELINUX for BIOS clients
 #sudo ln -svf /usr/share/syslinux/{pxelinux.0,menu.c32} /srv/tftpboot/
